afbc417b2026e0bcb21c67b56a1af0e843387c2c
Felix <farse028@uottawa.ca>
2024-07-03 13:18:41 -0400

    extracted helper method failed_to_participate? and refractored the code

--- Assignment2/grades/lib/grades/calculator.ex
+++ Assignment2/grades/lib/grades/calculator.ex
@@ -8,6 +8,11 @@ defmodule Grades.Calculator do
     end
   end
 
+  #added failed_to_participate? method to determine if the grades are low enough to fail
+  defp failed_to_participate?(avg_homework, avg_exams, num_labs) do
+    avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3
+  end
+
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
     avg_homework =
       avg(homework) #refractored all code where the average was calculated
@@ -33,7 +38,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if failed_to_participate?(avg_homework, avg_exams, num_labs) do #refractored all code where participation is calculated
       "EIN"
     else
       mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
@@ -68,7 +73,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       0
     else
       mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
